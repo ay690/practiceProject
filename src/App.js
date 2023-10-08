5112d6a5a2dea11483b86923106bc6eedf0aac80
Aniket Yadav <aniketyadav690@gmail.com>
2023-10-09 00:27:18 +0530

    shop cart basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from "./components/Products"
+import Cart from "./components/Cart"
+import { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { cartReducer } from "./reducers/cartReducer";
 
 function App() {
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+    cart: [],
+  
+  });
+
+
+  const fetchProducts = async() => {
+    const {data} = await axios.get("https://dummyjson.com/products");
+    // console.log(data.products);
+    dispatch({
+      type: "ADD_PRODUCTS",
+      payload: data.products
+    })
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-       hello
-      </header>
-    </div>
+  <div style={{display: "flex", }} >
+    <Products  state={state} dispatch={dispatch} />
+    <Cart state={state} dispatch={dispatch} />
+  </div>
   );
 }
 
