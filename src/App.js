2be228ce27f6708746cff3d903afa6cb4ea1f14f
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-25 01:59:03 +0530

    update; usestate removed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import './App.css';
-import Products from "./components/Products"
-import Cart from "./components/Cart"
-import { useState, useEffect, useReducer } from 'react';
-import axios from 'axios';
+import "./App.css";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
 import { cartReducer } from "./reducers/cartReducer";
 
 function App() {
-
   const [state, dispatch] = useReducer(cartReducer, {
     products: [],
     cart: [],
-  
   });
 
-
-  const fetchProducts = async() => {
-    const {data} = await axios.get("https://dummyjson.com/products");
+  const fetchProducts = async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
     // console.log(data.products);
     dispatch({
       type: "ADD_PRODUCTS",
-      payload: data.products
-    })
-  }
+      payload: data.products,
+    });
+  };
 
   useEffect(() => {
     fetchProducts();
-  }, [])
+  }, []);
 
   return (
-  <div style={{display: "flex", }} >
-    <Products  state={state} dispatch={dispatch} />
-    <Cart state={state} dispatch={dispatch} />
-  </div>
+    <div style={{ display: "flex" }}>
+      <Products state={state} dispatch={dispatch} />
+      <Cart state={state} dispatch={dispatch} />
+    </div>
   );
 }
 
